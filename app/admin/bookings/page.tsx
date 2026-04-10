e5074b57b43dd0785783ef85a2d9f6ddaaf90b10
bcx0 <battiste.crevieaux@icloud.com>
2026-04-10 09:53:53 +0200

    Fix closing tag for section in page.tsx

--- app/admin/bookings/page.tsx
+++ app/admin/bookings/page.tsx
@@ -79,4 +79,4 @@ export default async function AdminBookingsPage({ searchParams }: { searchParams
               <BookingsList bookings={serializedBookings} errorMessage={errorMessage} />
         </section>
       );
-}</section>
+}
